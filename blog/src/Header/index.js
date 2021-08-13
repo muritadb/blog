1456612dc74a8998a8791b3bd19561ef5c9ba2a7
muritadb <douglasb008@gmail.com>
2021-08-12 22:54:35 -0300

    inicio do projeto

---
+++ blog/src/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+    return (
+      <>
+         <header className="container-header">
+          <nav className="menu">
+            <ul>
+              <li>murita</li>
+              <li>hi </li>
+              <li>photos</li>            
+            </ul>
+          </nav>
+        </header>
+      </>
+    )
+}
+
+export default Header;
